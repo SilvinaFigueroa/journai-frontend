b8c438fafcf626d10fde69204354b1a38613af5f
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-07-26 14:15:27 -0700

    app protected  routes implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom'
+import LoginSignUp from './pages/login-signUp'
+import Journal from './pages/journal'
+import ProtectedRoutes from './components/ProtectedRoutes/auth'
 
 
 function App() {
 
   return (
     <>
- 
+      <Routes>
+        <Route path='login-signUp' element={<LoginSignUp />} />
+        <Route element={<ProtectedRoutes />}>
+          {/* Wrap protected content on the protected route */}
+          <Route path='journal' element={<Journal />} />
+        </Route>
+      </Routes>
     </>
   )
 }
